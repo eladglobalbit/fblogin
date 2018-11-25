7eb7701786ac16c0a6c042d0a3c286737f12a386
Elad Nisan <eladnisan@MacBook-Pro.mynet>
2018-11-25 15:33:02 +0200

    react-native-fbsdk-with android

---
+++ App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View ,TouchableOpacity} from 'react-native';
+import { LoginManager } from 'react-native-fbsdk';
+
+
+export default class App extends Component {
+
+
+  fbAuth() {
+    LoginManager.logInWithReadPermissions(["public_profile"]).then(
+      function(result) {
+        console.log(result)
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          console.log(
+            "Login success with permissions: " +
+              result.grantedPermissions.toString()
+          );
+        }
+      },
+      function(error) {
+        console.log("Login fail with error: " + error);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <View>
+      <TouchableOpacity onPress={this.fbAuth.bind(this)}>
+        <Text>Login with Facebook</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
